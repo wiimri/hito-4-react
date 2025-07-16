0d62cdc46473913c5f5095e27b72b357797c68c3
wiimri <williams.arias.q@gmail.com>
2025-07-16 03:08:10 -0400

    actualizacion index.js del backend

--- backend-pizza/index.js
+++ backend-pizza/index.js
@@ -1,24 +1,33 @@
-import cors from "cors";
-import "dotenv/config";
-import express from "express";
-
-import authRoute from "./routes/auth.route.js";
-import checkoutRoute from "./routes/checkout.route.js";
-import pizzaRoute from "./routes/pizza.route.js";
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
+const port = 5000;
 
-app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRoute);
-app.use("/api/pizzas", pizzaRoute);
-app.use("/api/checkouts", checkoutRoute);
-app.use((_, res) => {
-  res.status(404).json({ error: "Not Found" });
+const pizzas = [
+  {
+    id: "p001",
+    name: "napolitana",
+    desc: "La pizza napolitana, de masa tierna y delgada pero bordes altos...",
+    img: "../src/assets/img/pizza-napolitana.jpg",
+    ingredients: ["mozzarella", "tomates", "jamón", "orégano"],
+    price: 5950
+  },
+  {
+    id: "p002",
+    name: "margarita",
+    desc: "La pizza es una preparación culinaria...",
+    img: "../src/assets/img/pizza-margarita.jpg",
+    ingredients: ["queso", "salsa de tomate", "albahaca"],
+    price: 5500
+  }
+];
+
+app.get('/api/pizzas', (req, res) => {
+  res.json(pizzas);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
 });
